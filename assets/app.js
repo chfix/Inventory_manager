7479f2a79d27e8dbbb9cfbeb5489c796d73fedac
chfix <83886830+chfix@users.noreply.github.com>
2022-07-11 21:45:57 +0000

    datatables fix

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,4 @@ import './styles/app.scss';
 // start the Stimulus application
 import './bootstrap';
 
-// datatables
-import 'datatables.net';
-import 'datatables.net-bs5';
 
-$('#example').DataTable();
